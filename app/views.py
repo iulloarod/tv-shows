dea68c2440c7de135a24088944cbe7d611f61215
iUlloaRod <i.ulloarod@gmail.com>
2021-07-03 00:33:28 -0400

    ruteado y con templates creados

--- app/views.py
+++ app/views.py
@@ -2,5 +2,36 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
 
-    return render(request, 'index.html')
+    return redirect('/shows')
+
+
+def shows(request):
+    return render(request,"index.html")
+
+
+def new_show(request):
+    pass
+
+
+def create_show(request):
+    pass
+
+
+def show_id(request, id):
+    pass
+
+
+def show_edit(request, id):
+    pass
+
+
+def show_update(request, id):
+    pass
+
+
+def show_destroy(request, id):
+    pass
+
+
+
 
