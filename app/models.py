36630251109d41809fb1355e0b68bc3161d317ba
iUlloaRod <i.ulloarod@gmail.com>
2021-07-05 11:14:58 -0400

    plantilla de llegada show y /show/new están funcionando, pero con errores en datetime

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=99)
+    network = models.CharField(max_length=44)
+    rel_date = models.DateTimeField()
+    desc = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
--- app/views.py
+++ app/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render, HttpResponse, redirect
+from .models import Show
+from time import strftime, localtime
+import locale
+locale.setlocale(locale.LC_ALL, 'es-ES')
 
 def index(request):
-
     return redirect('/shows')
 
 
 def shows(request):
-    return render(request,"shows.html")
+    all_shows = Show.objects.all()
+    if request.method == "GET":
+        context= {
+            "all_shows": all_shows,
+        }
+    return render(request,"shows.html", context)
 
 
 def new_show(request):
-    return render(request,"new_show.html")
+    if request.method=='GET':
+        return render(request,"new_show.html")
 
 
 def create_show(request):
-    pass
+    if request.method=='POST':
+        request.POST['title']
+        request.POST['network']
+        request.POST['release_date']
+        request.POST['description']
+        print(request.POST)
+        Show.objects.create(title=request.POST['title'], 
+                            network=request.POST['network'], 
+                            rel_date=request.POST['release_date'],
+                            desc= request.POST['description']
+        )
+        return redirect('/shows')
 
 
 def show_id(request, id):
@@ -23,14 +43,16 @@ def show_id(request, id):
 
 def show_edit(request, id):
     pass
-
-
-def show_update(request):
     return render(request,"edit_show.html")
+
+
+def show_update(request, id):
+    return redirect('/shows/<id>')
 
 
 def show_destroy(request, id):
     pass
+    return redirect('/shows')
 
 
 
